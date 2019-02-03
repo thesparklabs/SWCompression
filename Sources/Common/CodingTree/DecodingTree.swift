e27a247a299e2fe25d1fa20949c21634f71a435d
Timofey Solomko <tsolomko@gmail.com>
2019-02-03 13:21:56 +0300

    [Implode] Add a version of DecodingTree.findNextSymbol() which checks if there are enough bits left on every processed bit
    Enabling this check for either Deflate or BZip2 would be extremely detrimental to their performance and such as it is implemented as a separate version of findNextSymbol function.

--- Sources/Common/CodingTree/DecodingTree.swift
+++ Sources/Common/CodingTree/DecodingTree.swift
@@ -47,4 +47,25 @@ final class DecodingTree {
         }
     }
 
+    /**
+     This function used in Implode algorithm instead of the usual `findNextSymbol`. The difference between them, is that
+     Implode's version checks for the end of data on every processed bit. It is necessary for Implode because the
+     algorithm doesn't define an end marker, and thus it is possible to encounter EOF while reading a new symbol. On the
+     other hand, this check is extremely expensive, and such it would be extremely detrimental to the performance of
+     both Defalte and BZip2 to enable it universally.
+     */
+    func implodeFindNextSymbol() -> Int {
+        var index = 0
+        while bitReader.bitsLeft >= 1 {
+            let bit = bitReader.bit()
+            index = bit == 0 ? 2 * index + 1 : 2 * index + 2
+            guard index < self.leafCount
+                else { return -1 }
+            if self.tree[index] > -1 {
+                return self.tree[index]
+            }
+        }
+        return -1
+    }
+
 }
